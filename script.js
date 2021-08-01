a3a6f948c68e35c5ecd89d9e2ace2fe84abd4259
Catherine Cox <ccox.034@gmail.com>
2021-07-31 21:08:43 -0600

    Started with adding the variables of alerting the user and prompting the user we are getting started as well as started developing the pseudo comments.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-JS;
 // Assignment Code
+// This is the start of the code, letting the user know we are gonna get started with asking the required Q's
+alert("Let's get started with your custom password!")
+
 var generateBtn = document.querySelector("#generate");
+var length = prompt("how many Charectors would you like your password to be?")
+var specialCharectors = confirm("Would you like to use Special Charectors?")
+var lowerCase = confirm("Would you like to use lowercase letters?")
+var upperCase = confirm("Would you like to use uppercase letters?")
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,6 +18,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// alert("Let's get started with your custom password!")
+// console.log
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
